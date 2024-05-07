b9569b200771a0804cc0e58ad899adfb83f30612
22CharactersInThisName <168596231+20CharacterInDisName@users.noreply.github.com>
2024-05-08 10:07:48 +1200

    Added file
    8/05/2024

---
+++ Functions practice.py
@@ -0,0 +1,11 @@
+#functinos dont go here
+def intro():
+    print("welcome")
+    print()
+    print("Instructions go here")
+    return "" #Returning all tiems in the code  
+
+# Main routine
+intro() #calls the function "Intro"
+
+
